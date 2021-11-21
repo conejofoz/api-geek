103d5f28429d5c018606902936db7852ba88b8b5
conejofoz <conejofoz@gmail.com>
2021-11-21 16:07:27 -0300

    Alterei as views para classes base views

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import AvaliacaoAPIView, CursoAPIView, CursoSerializer, AvaliacaoSerializer
+from .views import AvaliacaoAPIView, AvaliacoesAPIView, CursoAPIView, CursosAPIView, CursoSerializer, AvaliacaoSerializer
 
 urlpatterns = [
-    path('cursos/', CursoAPIView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes'),
+    path('cursos/', CursosAPIView.as_view(), name='cursos'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('cursos/<int:pk>/', CursoAPIView.as_view(), name='curso'),
+    path('avaliacoes/<int:pk>/', AvaliacaoAPIView.as_view(), name='avaliacao'),
 ]
