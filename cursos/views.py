f0ac5b5fe97f7e65795c4e32549b3624fe9210d5
conejofoz <conejofoz@gmail.com>
2021-11-22 18:12:29 -0300

    sobrescrita dos métodos genéricos

--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,8 @@
-from django.db.models.query import QuerySet
-from rest_framework import generics, serializers
+#from django.db.models.query import QuerySet
+#from rest_framework import generics, serializers
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -19,8 +22,20 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
+
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return generics.get_object_or_404(self.get_queryset(),curso_id=self.kwargs.get('curso_pk'),pk=self.kwargs.get('avaliacao_pk'))
+        return generics.get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+
     
