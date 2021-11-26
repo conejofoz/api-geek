667d0c97a3552dbd1eb1cbc3a02b638ced2db062
conejofoz <conejofoz@gmail.com>
2021-11-25 21:13:33 -0300

    Paginação com Django Rest Framework

--- escola/settings.py
+++ escola/settings.py
@@ -136,12 +136,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-#DRF
+# DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication', 
     ),
     'DEFAULT_PERMISSION_CLASSES':(
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2 # Se sobrescrevermos os métodos na view essa configuração é anulada
 }
