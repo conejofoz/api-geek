b0a14d5902a8dca38f057a988f3ffc5d7e041ee3
Silvio Coelho <conejofoz@gmail.com>
2021-12-02 15:21:59 -0300

    trabalhando com permissions

--- cursos/views.py
+++ cursos/views.py
@@ -2,13 +2,15 @@
 #from rest_framework import generics, serializers
 from django.db.models.fields import mixins
 from rest_framework import generics, serializers
-from rest_framework.decorators import action
+from rest_framework.decorators import action, permission_classes
 from rest_framework.generics import get_object_or_404
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import mixins
+
+from rest_framework import permissions
 
 
 from .models import Curso, Avaliacao
@@ -57,6 +59,11 @@ API V2
 
 
 class CursoViewSet(viewsets.ModelViewSet):
+    # se sobrepôe a configuração do settings.py somente para essa classe
+    permission_classes = (
+        permissions.DjangoModelPermissions,
+
+        )
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
